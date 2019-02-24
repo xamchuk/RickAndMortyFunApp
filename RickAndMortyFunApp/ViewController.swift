d911fe93e5db521a905e4b200419844b81195d38
xamchuk <30704986+xamchuk@users.noreply.github.com>
2019-02-24 10:52:05 +0000

    Initial commit

---
+++ RickAndMortyFunApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyFunApp
+//
+//  Created by Rusłan Chamski on 24/02/2019.
+//  Copyright © 2019 Rusłan Chamski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
