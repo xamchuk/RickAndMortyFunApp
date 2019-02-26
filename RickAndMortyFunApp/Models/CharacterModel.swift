c1d42cb4986e404a195681358d4840c7dd700714
Rusłan Chamski <newline_promo@ukr.net>
2019-02-26 09:12:41 +0000

    json model implemented

---
+++ RickAndMortyFunApp/Models/CharacterModel.swift
@@ -0,0 +1,33 @@
+//
+//  CharacterModel.swift
+//  RickAndMortyFunApp
+//
+//  Created by Rusłan Chamski on 26/02/2019.
+//  Copyright © 2019 Rusłan Chamski. All rights reserved.
+//
+
+import Foundation
+
+struct Character: Codable {
+    var id: Int
+    var name: String
+    var status: String
+    var species: String
+    var type: String
+    var gender: String
+    var origin: Origin
+    var location: Location
+    var image: String
+    var episode: [String]
+    var url: String
+}
+
+struct Origin: Codable {
+    var name: String
+    var url: String
+}
+
+struct Location: Codable {
+    var name: String
+    var url: String
+}
